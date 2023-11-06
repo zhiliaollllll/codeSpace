c1838062554d8ae469a19ad7b101c53f87aa3ed9
lium222mmm <2569514032@qq.com>
2023-11-06 12:50:00 +0800

    视频倍速

--- html/video/index.js
+++ html/video/index.js
@@ -1,5 +1,17 @@
 var speed = document.querySelector('.speed')
+var bar = document.querySelector('.speed-bar')
+var video = document.querySelector('.flex')
 // console.log(speed);
 speed.addEventListener('mousemove', function (e) {
-    console.log(e.y);
+    var y = e.y - speed.offsetTop;
+    var percent = y / speed.offsetHeight;//读到一个容器的高度
+    var height = Math.round(percent * 100) + '%';
+    bar.style.height = height;
+    var min = 0.4
+    var max = 3
+    var playbackRate = percent * (max - min) + min
+    bar.textContent = playbackRate.toFixed(2) + 'x'
+    // console.log(height);
+    //视频播放速度
+    video.playbackRate = playbackRate
 })
