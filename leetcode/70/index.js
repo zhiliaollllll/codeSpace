932946d137e0d5217ed5a90a3e259bee44a13a1e
lium222mmm <2569514032@qq.com>
2023-11-13 16:30:41 +0800

    leetcode

---
+++ leetcode/70/index.js
@@ -0,0 +1,11 @@
+var climbStairs = function (n) {
+    const a = []
+    a[1] = 1
+    a[2] = 2
+    if (let i = 3; i <= n; i++) {
+        a[i] = a[i - 1] + a[i - 2]
+    }
+    return a[n]
+};
+
+climbStairs(4)
